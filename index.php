9946e9e9ea6261f8574ff9c34f13cc6cf5c25408
Justin Bond <bond.justink@gmail.com>
2018-01-28 19:19:08 -0800

    Initial setup

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+include_once('settings.php');// include the account info variables
+include_once('quickbase.php');//include the api file
---
+++ settings-example.php
@@ -0,0 +1,8 @@
+<?php
+
+// Variables to hold account info
+$qbUser     	= ''; // quickbase username
+$qbPassword 	= ''; // quickbase password
+$qbDbId 		= ''; // quickbase database ID
+$qbUserToken 	= ''; // quickbase user token
+$qbAppToken 	= ''; // quickbase app token

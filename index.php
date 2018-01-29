f50261b3a8c9ba9679695d91ea5f71cdade74409
Justin Bond <bond.justink@gmail.com>
2018-01-28 19:48:53 -0800

    added query call

--- index.php
+++ index.php
@@ -2,3 +2,25 @@
 
 include_once('settings.php');// include the account info variables
 include_once('quickbase.php');//include the api file
+
+$qb = new QuickBase($qbUser, $qbPassword, $qbUserToken, true, $qbDbId, $qbAppToken, $qbRealm, '');//creating the object
+
+$queries = array(
+		array(
+			'fid'  => '46', // Feild ID
+			'ev'   => 'EX', // Exact
+			'cri'  => (strtotime('yesterday midnight')*1000) // criteria
+		),
+		array(
+			'ao'  => 'AND',//OR is also acceptable
+			'fid' => '57',
+			'ev'  => 'EX',
+			'cri' => 'customer'
+		)
+	);
+
+$results = $qb->do_query($queries, '', '', 'a', '20', 'structured', 'sortorder-A'); // receive all fields
+
+foreach($results->table->records->record as $record) {
+	echo '<pre>'; print_r($record); echo '</pre>';
+}
--- settings-example.php
+++ settings-example.php
@@ -3,6 +3,7 @@
 // Variables to hold account info
 $qbUser     	= ''; // quickbase username
 $qbPassword 	= ''; // quickbase password
+$qbRealm 		= ''; // quickbase realm
 $qbDbId 		= ''; // quickbase database ID
 $qbUserToken 	= ''; // quickbase user token
 $qbAppToken 	= ''; // quickbase app token

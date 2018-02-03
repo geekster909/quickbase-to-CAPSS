e1400190b41b0bac5967b8bbdfdd795a87c89df4
Justin Bond <bond.justink@gmail.com>
2018-02-02 17:02:54 -0800

    added create the xml with data

--- ajax-get-transactions.php
+++ ajax-get-transactions.php
@@ -76,7 +76,8 @@ if ( !isset($location) ) {
 		// set the variables for the results
 		$recordId = $record[0];
 		$customerFullName = $record[1];
-		$transactionDate = gmdate("m-d-Y", $record[2] / 1000);
+		$transactionDateSeconds = $record[2] / 1000;
+		$transactionDate = gmdate("m-d-Y", $transactionDateSeconds);
 		$employeeFullName = $record[4];
 
 		// convert time of day from seconds to hours:mins:seconds
@@ -119,6 +120,11 @@ if ( !isset($location) ) {
 		$eyeColorOptions = array ('Black', 'Blue', 'Brown', 'Gray', 'Hazel', 'Pink', 'Green', 'Multi Color', 'Unknown');
 		$eyeColor = in_array($customerResults[9],$eyeColorOptions) ? $customerResults[9] : 'Brown';
 
+		// set a default height if there is not one
+		$heightFeet = is_array($customerResults[10]) ? '5' : $customerResults[10];
+		$heightInches = is_array($customerResults[11]) ? '7' : $customerResults[11];
+		$weight = is_array($customerResults[12]) ? '175' : $customerResults[12];
+
 		// set the customer information for the transaction
 		$transactionInfo['customerInfo'] = array(
 			'firstName' => $customerResults[0],
@@ -131,9 +137,9 @@ if ( !isset($location) ) {
 			'gender' => $customerResults[7],
 			'hairColor' => $hairColor,
 			'eyeColor' => $eyeColor,
-			'heightFeet' => $customerResults[10],
-			'heightInches' => $customerResults[11],
-			'weight' => $customerResults[12],
+			'heightFeet' => $heightFeet,
+			'heightInches' => $heightInches,
+			'weight' => $weight,
 			'identificationType' => $customerResults[13],
 			'idNumber' => $customerResults[14],
 		);
@@ -153,7 +159,23 @@ if ( !isset($location) ) {
 
 
 		$return_array['transactions'][$transactionInfo['recordId']] = $transactionInfo;
+		$xmlDoc = create_xml($transactionInfo);
+		//make the output pretty
+		$xmlDoc->formatOutput = true;
+		// print_r($xmlDoc->saveXML());die();
 
+		$filePath = 'xml/'.gmdate("Y", $transactionDateSeconds).'/'.gmdate("m", $transactionDateSeconds).'/'.gmdate("d", $transactionDateSeconds);
+
+		if (!file_exists($filePath)) {
+	    	mkdir($filePath, 0777, true);
+		}
+
+		$xmlDoc->save($filePath."/".$transactionInfo['recordId'].".xml");
+		// if ($xmlDoc->save($filePath."/".$recordInfo['recordId'].".xml")) {
+		// 	echo 'Saved Record ID '.$recordInfo['recordId'].' to '.$filePath.'<br/><br/>';
+		// } else {
+		// 	echo 'Did not save Record ID '.$recordInfo['recordId'].'<br/><br/>';
+		// }
 
 	}
 }
@@ -161,3 +183,123 @@ if ( !isset($location) ) {
 
 echo json_encode($return_array);
 return;
+
+
+function create_xml($transactionInfo) {
+	// header("Content-Type: text/plain");
+
+	/*
+		REQUIRED FIELDS
+
+		CUSTOMER INFO
+			Last Name
+			First Name
+			DOB mm/dd/yyyy
+			Address
+			City
+			State "California"
+			Postal Code
+			Gender (Male, Female)
+			Hair Color (Bald, Black, Blond, Brown, Gray, Red, Sandy, White)
+			Eye Color (Black, Blue, Browm, Gray, Hazel, Pink, Green, Multi Color)
+			Height (ft.)
+			Height (in.)
+			Weight
+			Indentification Type (Drivers License, Passport, State Id, Military Id, Matricula Consular, United States Id)
+			ID Number
+		
+		STORE INFO
+			Store Name
+			License Number
+			Law Enforcement Agency
+			Address
+			City
+			State
+			Postal Code
+			Store Phone Number
+			Employee Name
+			Employee Signiture (image)
+
+		TRANSACTION ITEMS
+			Transaction Date (mm/dd/yyy)
+			Transaction Time (hh:mm AM/PM)
+
+		ITEM(S)
+			Type (Buy, Consign, Trade, Auction)
+			Article
+			Brand Name
+			Loan/Buy Number
+			$ Amount
+			Property Description (One Item Only, Size, Color, Material, etc...)
+
+		SIGNITURE
+			Customer Signiture (image)
+			Customer Thumbprint (image)
+	*/
+
+	//create the xml document
+	$xmlDoc = new DOMDocument('1.0', 'UTF-8');
+
+	$capssUpload = $xmlDoc->appendChild(
+		$xmlDoc->createElement('capssUpload'));
+
+	$capssUpload->appendChild(
+    	$xmlDoc->createAttribute("xmlns:xsd"))->appendChild(
+      		$xmlDoc->createTextNode("http://www.w3.org/2001/XMLSchema"));
+
+    $capssUpload->appendChild(
+    	$xmlDoc->createAttribute("xmlns:xsi"))->appendChild(
+      		$xmlDoc->createTextNode("http://www.w3.org/2001/XMLSchema-instance"));
+
+    $bulkUploadData = $capssUpload->appendChild(
+    	$xmlDoc->createElement("bulkUploadData"));
+    $bulkUploadData->appendChild(
+    	$xmlDoc->createAttribute("licenseNumber"))->appendChild(
+      		$xmlDoc->createTextNode("01081001"));
+
+	$propertyTransaction = $bulkUploadData->appendChild(
+    	$xmlDoc->createElement("propertyTransaction"));
+
+	$transactionTime = $propertyTransaction->appendChild(
+    	$xmlDoc->createElement("transactionTime", gmdate('Y-m-d', $transactionInfo['transactionDate']).'T'.$transactionInfo['transactionTime']));
+
+	$customer = $propertyTransaction->appendChild(
+    	$xmlDoc->createElement("customer"));
+	$custLastName = $customer->appendChild(
+    	$xmlDoc->createElement("custLastName", $transactionInfo['customerInfo']['lastName']));
+	$custFirstName = $customer->appendChild(
+    	$xmlDoc->createElement("custFirstName", $transactionInfo['customerInfo']['firstName']));
+	$custMiddleName = $customer->appendChild(
+    	$xmlDoc->createElement("custMiddleName", $transactionInfo['customerInfo']['middleName']));
+	$gender = $customer->appendChild(
+    	$xmlDoc->createElement("gender", $transactionInfo['customerInfo']['gender']));
+	$race = $customer->appendChild(
+    	$xmlDoc->createElement("race", $transactionInfo['customerInfo']['race']));
+	$hairColor = $customer->appendChild(
+    	$xmlDoc->createElement("hairColor", $transactionInfo['customerInfo']['hairColor']));
+	$eyeColor = $customer->appendChild(
+    	$xmlDoc->createElement("eyeColor", $transactionInfo['customerInfo']['eyeColor']));
+	$height = $customer->appendChild(
+    	$xmlDoc->createElement("height", $transactionInfo['customerInfo']['heightFeet']."' ".$transactionInfo['customerInfo']['heightInches'].'"'));
+	$weight = $customer->appendChild(
+    	$xmlDoc->createElement("weight", $transactionInfo['customerInfo']['weight']));
+		$weight->appendChild(
+	    	$xmlDoc->createAttribute("unit"))->appendChild(
+	      		$xmlDoc->createTextNode("pounds"));
+	$dateOfBirth = $customer->appendChild(
+    	$xmlDoc->createElement("dateOfBirth", $transactionInfo['customerInfo']['dob']));
+	$streetAddress = $customer->appendChild(
+    	$xmlDoc->createElement("streetAddress", $transactionInfo['customerInfo']['address']));
+	$city = $customer->appendChild(
+    	$xmlDoc->createElement("city", $transactionInfo['customerInfo']['city']));
+	$state = $customer->appendChild(
+    	$xmlDoc->createElement("state", $transactionInfo['customerInfo']['state']));
+	$postalCode = $customer->appendChild(
+    	$xmlDoc->createElement("postalCode", $transactionInfo['customerInfo']['postalCode']));
+	$phoneNumber = $customer->appendChild(
+    	$xmlDoc->createElement("phoneNumber", $transactionInfo['customerInfo']['phoneNumber']));
+
+
+
+    return $xmlDoc;
+}
